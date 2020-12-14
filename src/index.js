51ff7d2b0caf3068d29f4a2aa10c90e2e99b8a48
Jade Lynn Masker <jadelynn@masker.family>
2020-12-13 20:55:06 -0500

    commit tree 6b4835e8821af8ec28a2d3948fdd228104414850

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-let addToy = false;
 
-document.addEventListener("DOMContentLoaded", () => {
-  const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
-  addBtn.addEventListener("click", () => {
-    // hide & seek with the form
-    addToy = !addToy;
-    if (addToy) {
-      toyFormContainer.style.display = "block";
-    } else {
-      toyFormContainer.style.display = "none";
-    }
-  });
+function fetchToys() {
+	return fetch(
+		'http://localhost:3000/toys'
+	).then(function(response) {
+		return response.json();
+	});
+}
+
+function makeCard(toy) {
+	const card = document.createElement('div');
+	card.class = 'card'
+	const name = document.createElement('h2');
+	name.innerText = toy.name;
+	const image = document.createElement('img');
+	image.src = toy.image.src;
+	const likes = document.createElement('p');
+	likes.innerText = '0';
+	const likeButton = document.createElement('button');
+	likeButton.class = 'like-btn';
+	likeButton.innerText = 'like'
+}
+
+function addToysToToyCollection(toyCollection) {
+	fetchToys().then(function(toys) {
+		for (toy of toys) {
+			const card = makeCard(toy);
+		}
+	});
+	return toyCollection;
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+	const toyCollection = addToysToToyCollection(document.getElementById('toy-collection'));
 });
